36ae6bf3b8ee0435a95d270830ff4155f077b792
Sandeep gupta <sivistinsam2@gmail.com>
2020-07-18 17:51:16 -0700

    Module4_Solution

---
+++ Module4_Solution/script.js
@@ -0,0 +1,13 @@
+(function (window) {
+
+var names = ["sam", "tom", "joe", "tim", "ray", "seth", "cloe", "tony", "harry", "jhon"];
+for (var i in names) {
+  var firstLetter = ((names[i]).charAt(0)).toLowerCase();
+
+  if (firstLetter == 'j') {
+     window.byeSpeaker.speak(names[i]);
+  } else {
+     window.helloSpeaker.speak(names[i]);
+  }
+}
+})(window);
